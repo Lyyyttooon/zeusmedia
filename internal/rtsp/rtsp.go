a816a6974ddbf9a5c5827fa476fc07361a58c298
Lyyyttooon <bnn0202@outlook.com>
2023-04-19 16:31:31 +0800

    feat: 增加logger以及tcp连接

--- internal/rtsp/rtsp.go
+++ internal/rtsp/rtsp.go
@@ -1,19 +1,26 @@
 package rtsp
 
-import "github.com/Lyyyttooon/zeusmedia/utils"
+import (
+	"net"
+
+	"github.com/Lyyyttooon/zeusmedia/utils"
+)
 
 type RTSP struct {
 	rawURL string
 	urlCtx utils.URLContext
+
+	conn net.Conn
 }
 
-// NewPullSession 新建
+// NewPullSession 新建RTSP结构体
 func NewPullSession(url string) RTSP {
 	return RTSP{
 		rawURL: url,
 	}
 }
 
+// Conn 连接
 func (r *RTSP) Conn() {
 	errChan := make(chan error, 1)
 
@@ -24,11 +31,20 @@ func (r *RTSP) Conn() {
 	}()
 }
 
+// connect 建立连接
 func (r *RTSP) connect() (err error) {
 	r.urlCtx, err = utils.ParseRTSPURL(r.rawURL)
 	if err != nil {
 		return err
 	}
+	utils.SLogger.Debugf("[%s] > tcp connect.", r.rawURL)
+
+	// 建立连接
+	conn, err := net.Dial("tcp", r.urlCtx.HostWithPort)
+	if err != nil {
+		return err
+	}
+	r.conn = conn
 
 	return nil
 }
